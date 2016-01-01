f37945c59228d3b8f876752b37bce9cb08b14191
Arashshshsh <arashmardan.am@gmail.com>
2016-01-01 12:54:06 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {110626080,103649648,0,tonumber(our_id)},--Sudo users
+    sudo_users = {109722284},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
