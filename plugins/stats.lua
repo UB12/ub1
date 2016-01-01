f6b08f5d8a60b63a52d3ee8c44539d12d409cfa7
Arashshshsh <arashmardan.am@gmail.com>
2016-01-01 12:58:28 -0800

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -141,8 +141,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (infernal)",-- Put everything you like :)
+		"^[!/]([Ii]nfernalteam)"-- Put everything you like :)
     }, 
   run = run
 }
