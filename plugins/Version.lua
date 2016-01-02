4e933764f4116069fd28c8cfac8b2446c337ab18
Arashshshsh <arashmardan.am@gmail.com>
2016-01-02 06:58:10 -0800

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,31 @@
+do
+
+function run(msg, matches)
+  return 'INFERNAl team robot v.1'.. [[ 
+website :
+soon 😍
+Sudo : @ArashINFERNAL 😊
+For Gb Admin :
+50tomans
+for 3 month 😱
+👑íղƒҽɾղɑӀ👑 ɾҽԹօɾԵ
+👑íղƒҽɾղɑӀ👑TEAM
+admins :
+@creed_is_dead 
+@Mustafaflux
+for global admin : 40 tommans for 2 month🙈
+or 20tomans each month😝
+The Will open source soon
+Enigneer ᗩᖇᗩᔕᕼ.Iᑎᖴᕮᖇᑎᗩᒪ .]] 
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows infernal robot version",
+  patterns = {
+    "^!ver$"
+  }, 
+  run = run 
+}
+
+end
