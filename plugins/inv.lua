bd00ee20a8ba13834ea98f279c1c382f02e3842c
Arash-SiS <arashmardan.am@gmail.com>
2016-01-03 16:50:25 -0800

    Rename invite.lua to inv.lua

---
+++ plugins/inv.lua
@@ -0,0 +1,49 @@
+-- Invite other user to the chat group.
+-- Use !invite name User_name or !invite id id_number
+-- The User_name is the print_name (there are no spaces but _)
+
+do
+
+local function callback(extra, success, result)
+  vardump(success)
+  vardump(result)
+end
+
+local function run(msg, matches)
+  local user = matches[2]
+
+  -- User submitted a user name
+  if matches[1] == "name" then
+    user = string.gsub(user," ","_")
+  end
+  
+  -- User submitted an id
+  if matches[1] == "code" then
+    user = 'user#code'..user
+  end
+
+  -- The message must come from a chat group
+  if msg.to.type == 'chat' then
+    local chat = 'chat#id'..msg.to.id
+    chat_add_user(chat, user, callback, false)
+    return "Add: "..user.." to "..chat
+  else 
+    return 'This isnt a chat group!'
+  end
+
+end
+
+return {
+  description = "Invite other user to the chat group", 
+  usage = {
+    "!invite name [user_name]", 
+    "!invite code [user_code]" },
+  patterns = {
+    "^!invite (name) (.*)$",
+    "^!invite (code) (%d+)$"
+  }, 
+  run = run,
+  moderation = true 
+}
+
+end
