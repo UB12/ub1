64bf0e00387c08be6240b54c594162811a1af714
Arash-SiS <arashmardan.am@gmail.com>
2016-01-04 03:26:24 -0800

    Create LockTag.lua

---
+++ plugins/LockTag.lua
@@ -0,0 +1,12 @@
+local function run(msg)
+    if not is_momod(msg) and msg.type = 'chat' then
+        --chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
+        return 
+    end
+end
+
+return {patterns = {
+    "#",
+    },
+    run = run
+    }
