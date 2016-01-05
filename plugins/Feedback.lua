ff7292cc7a3a63287066070f2b9075414a65665f
sahebankherad <arsalangorbanzade2013@gmail.com>
2016-01-05 16:32:23 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,9 +19,9 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#82481072', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#70690378', fuse, ok_cb, false)
 
- return 'Your request has been sended to @ArashINFERNAl 😜!'
+ return 'Your request has been sended to @Creed_is_dead and Team 😜!'
 
      
 
